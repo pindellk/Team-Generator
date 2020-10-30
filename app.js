61ee007b5369decd091eeba6e15246d90a03e171
pindellk <pindellk@protonmail.com>
2020-10-30 17:08:22 -0400

    Add HTML renderer with employee array

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Create dependencies 
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -10,10 +11,10 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// Empty employee array
 const employeeList = [];
 
+// Main questions array
 const questions = [
     {
         type: "input",
@@ -39,6 +40,7 @@ const questions = [
     }
 ];
 
+// Employee specific questions
 const managerQuestion = {
     type: "number",
     name: "officeNumber",
@@ -64,6 +66,7 @@ const additionalEmployee = {
     message: "Would you like to enter more employees?"
 }
 
+// Prompt user for employee data; divert path based on selected role 
 function addEmployee() {
     inquirer
         .prompt(questions)
@@ -99,45 +102,19 @@ function addEmployee() {
                             if (answer.add === true) {
                                 addEmployee();
                             }
+                            else {
+                                const html = render(employeeList);
+                                fs.writeFileSync(outputPath, html, (err) => {
+                                    if (err)
+                                        console.log(err);
+                                    else {
+                                        console.log("File written successfully\n");
+                                    }
+                                });
+                            }
                         })
-                    console.log(employeeList);
                 })
         })
 }
 
 addEmployee();
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-
-// render()
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-// function addEngineer(answers) {
-//     inquirer.prompt({
-//         type: "input",
-//         name: "github",
-//         message: "What is your GitHub account?"
-//     }).then((answer) => {
-//         Object.assign(answers, answer);
-//         const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github, answers.role);
-//     });
-// }
